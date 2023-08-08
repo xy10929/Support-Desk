3369ea345fa3ed4389d714c8183b9657b165459c
Zhengyu Yang <yangzy0105@gmail.com>
2023-08-07 22:16:49 -0700

    8.7

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log('Server...')
+//import express
+const express = require('express')
+
+//import .env
+const dotenv = require('dotenv').config()
+
+//find port in .env
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+//create route
+
+//root url
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' })
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
