f6580085a90fb8d2eefc391abeeacf247a3ad44a
Zhengyu Yang <yangzy0105@gmail.com>
2023-08-10 00:08:51 -0700

    8.10

--- backend/server.js
+++ backend/server.js
@@ -16,4 +16,8 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API' })
 })
 
+//route
+///POST api/users/... matches the url in userRoute.js
+app.use('/api/users', require('./routes/userRoutes'))
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
