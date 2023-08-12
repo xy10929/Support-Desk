0582c1c25355d30de372a50e056dc87add229034
Zhengyu Yang <yangzy0105@gmail.com>
2023-08-12 12:30:50 -0700

    8.12

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,11 @@
 //handle promise (by mongoose) easier
 const asyncHandle = require('express-async-handler')
 
+//hash password
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/userModel')
+
 // @desc    Register a new user
 // @route   /api/users
 // @access  Public
@@ -13,7 +18,33 @@ const registerUser = asyncHandle(async (req, res) => {
     throw new Error('Please include all fields')
   }
 
-  res.send('Register Route')
+  //find if user already exists
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  //hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  //creat user with req
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new error('Incalid user data')
+  }
 })
 
 // @desc    Login a user
