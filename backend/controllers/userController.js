ee497043b51b54351c236997f91d9cfa94307cf4
Zhengyu Yang <yangzy0105@gmail.com>
2023-08-13 04:48:32 -0700

    8.13

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,17 @@
 //handle promise (by mongoose) easier
-const asyncHandle = require('express-async-handler')
+const asyncHandler = require('express-async-handler')
 
 //hash password
 const bcrypt = require('bcryptjs')
+
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel')
 
 // @desc    Register a new user
 // @route   /api/users
 // @access  Public
-const registerUser = asyncHandle(async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
 
   //validation
@@ -40,6 +42,7 @@ const registerUser = asyncHandle(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
@@ -50,11 +53,45 @@ const registerUser = asyncHandle(async (req, res) => {
 // @desc    Login a user
 // @route   /api/users/login
 // @access  Public
-const loginUser = asyncHandle(async (req, res) => {
-  res.send('Login Route')
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+  const user = await User.findOne({ email })
+
+  //check user and password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else {
+    //unauthorized
+    res.status(401)
+    throw new Error('Invalid credentials')
+  }
 })
+
+// @desc    get current user
+// @route   /api/users/me
+// @access  Public
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  }
+  res.status(200).json(user)
+})
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
 
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = express.Router()
 //functions import from userController.js
-const { registerUser, loginUser } = require('../controllers/userController')
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require('../controllers/userController')
+
+const { protect } = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
 
 router.post('/login', loginUser)
+
+router.get('/me', protect, getMe)
 
 //export router
 module.exports = router
