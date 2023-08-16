4bf3d13501e542ddd2bfb3327dcd54c27842a9d4
Zhengyu Yang <yangzy0105@gmail.com>
2023-08-16 04:33:42 -0700

    8.16

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 function App() {
-  return <div>My App</div>
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          {/* navigate between the routes and their components  */}
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  )
 }
 
 export default App
