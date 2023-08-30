b9048019c9401e64212ab5fe3ae1b9489be4350a
Zhengyu Yang <yangzy0105@gmail.com>
2023-08-30 09:24:44 -0700

    8.30

--- frontend/src/components/NoteItem.jsx
+++ frontend/src/components/NoteItem.jsx
@@ -12,7 +12,8 @@ function NoteItem({ note }) {
       }}
     >
       <h4>
-        Note from {note.isStuff ? <span>Staff</span> : <span>{user.name}</span>}
+        Note from{' '}
+        {note.isStaff ? <span> staff xy </span> : <span>{user.name}</span>}
       </h4>
       <p>{note.text}</p>
       <div className='note-date'>
